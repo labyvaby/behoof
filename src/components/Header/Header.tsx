7f287f708b71b007d0b252ae9053bfa57abd00b4
labyvaby <dipt688@gmail.com>
2025-08-04 20:16:46 +0600

    add components header and container and also logo.svg

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../Сontainer/Container';
+import logo from '../../assets/logo.svg';
+import './style.scss';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Container>
+        <img src={logo} alt="" />
+      </Container>
+    </header>
+  );
+}
+
