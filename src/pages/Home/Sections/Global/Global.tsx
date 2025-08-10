213f3c73b1441d491b5a2ed7e4326ae9044f7f67
baiel <raimkulovbaiel2@gmail.com>
2025-08-10 16:19:07 +0600

    refact

--- src/pages/Home/Sections/Global/Global.tsx
+++ src/pages/Home/Sections/Global/Global.tsx
@@ -3,7 +3,7 @@ import './style.scss';
 
 const Global: React.FC = () => {
   const stats = [
-    { number: '5', text: 'Lorem ipsum dolor sit amet consectetur. Rhoncus risus nunc a pharetra viverra enim nunc.' },
+    { number: '5ь', text: 'Lorem ipsum dolor sit amet consectetur. Rhoncus risus nunc a pharetra viverra enim nunc.' },
     { number: '30', text: 'Gravida vel convallis id aliquet volutpat nullam dignissim. Amet parturient elementum lectus rhoncus at.' },
     { number: '300', text: 'Sed varius ut venenatis id amet et consectetur pellentesque. Vitae uo ornare vel suspendisse tincidunt.' },
     { number: '8', text: 'Id enim ornare lacus duis. Ac fermentum auctor cras adipiscing feugiat quis convollis velit.' }
