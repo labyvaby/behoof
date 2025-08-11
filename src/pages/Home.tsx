abe9e05c7c59ea082254747bb4d47bf0ad40edf0
baiel <raimkulovbaiel2@gmail.com>
2025-08-11 23:57:30 +0600

    added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import BestChoice from './Home/Sections/BestChoice/BestChoice';
 import ProductCardList from './Home/Sections/ProductCardList/ProductCardList';
 import Global from './Home/Sections/Global/Global';
-import Banner from './Home/Sections/Banner/Banner';
+import Banner from './Home/Sections/Banner/Banner'; 
 export default function Home() {
   return (
     <div>
       <BestChoice />
       <ProductCardList />
       <Global />
-      <Banner />
-
+      <Banner />   
+      
     </div>
   );
 }
---
+++ src/pages/Home/Sections/NewProducts/NewProducts.tsx
@@ -0,0 +1,8 @@
+import './style.scss'
+
+ const NewProducts = () => {
+  return (
+    <div>NewProducts</div>
+  )
+}
+export default NewProducts
