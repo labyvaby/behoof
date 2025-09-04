e46dbd6a5b01b1266cca185aa0df63183f7d9eb4
baiel <raimkulovbaiel2@gmail.com>
2025-09-04 19:49:13 +0600

    added Testimonials

--- src/pages/Productgroup.tsx
+++ src/pages/Productgroup.tsx
@@ -1,10 +1,12 @@
  import SmartphonesPage from './Productgroup/Sections/SmartphonesPage/SmartphonesPage';
   import ProductList from './Productgroup/Sections/ProductList/ProductList';
+import Testimonials from './Productgroup/Sections/Testimonials/Testimonials';
   export const Productgroup = () => {
   return (  
     <div> 
            <SmartphonesPage/>
-            <ProductList/>
+            <ProductList/> 
+            <Testimonials/>
     </div> 
 
   )
---
+++ src/pages/Productgroup/Sections/Testimonials/Testimonials.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import "./style.scss";
+
+interface TestimonialsItem {
+    id: number;
+    image: string;
+    title: string;
+    text: string;
+}
+
+const Testimonials: React.FC = () => {
+    const [reviews, setReviews] = useState<TestimonialsItem[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/Testimonials")
+            .then((res) => res.json())
+            .then((data) => setReviews(data))
+    }, []);
+
+    return (
+        <section className="container">
+            <div className="container__header">
+                <h2>Отзывы</h2>
+                <a href="/" className="container__link">
+                    Смотреть все →
+                </a>
+            </div>
+
+            <div className="reviews-grid">
+                {reviews.map((item) => (
+                    <article className="review-card" key={item.id}>
+                        <div className="review-card__image-wrap">
+                            <img
+                                src={item.image}
+                                alt={item.title}
+                                className="review-card__image"
+                                loading="lazy"
+                            />
+                        </div>
+                        <div className="review-card__content">
+                            <h3 className="review-card__title">{item.title}</h3>
+                            <p className="review-card__text">{item.text}</p>
+                            <a href="/" className="review-card__link">
+                                Смотреть
+                                <img
+                                    src="src/assets/icons/Review/iconc- Right 2.svg"
+                                    className="review-card__icon"
+                                    alt="logo"
+                                />
+                            </a>
+                        </div>
+                    </article>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Testimonials;
