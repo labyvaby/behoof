2d4424dad1d59e7d687aaabf41eeab1035a85842
baiel <raimkulovbaiel2@gmail.com>
2025-09-14 00:16:39 +0600

    added ReviewsSection

--- src/pages/ProductInfo.tsx
+++ src/pages/ProductInfo.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
  import ProductPage from './ProductInfo/Sections/ProductPage/ProductPage';
+import ReviewsSection from './ProductInfo/Sections/ReviewsSection/ReviewsSection';
 
 const ProductInfo = () => {
   return (
     <div> 
-         <ProductPage/>
+         <ProductPage/> 
+         <ReviewsSection/>
     </div>
   )
 } 
