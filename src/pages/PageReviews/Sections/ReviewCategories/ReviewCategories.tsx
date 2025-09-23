74f20c40f44788a03f2523cb55c1f068dd255fe6
baiel <raimkulovbaiel2@gmail.com>
2025-09-23 23:16:08 +0600

    added-db.json-ReviewCategories

--- src/pages/PageReviews/Sections/ReviewCategories/ReviewCategories.tsx
+++ src/pages/PageReviews/Sections/ReviewCategories/ReviewCategories.tsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 
-const categories = [
-  { title: "Смартфоны", img: "src/assets/images/ReviewCategories/Imag1.png" },
-  { title: "Ноутбуки", img: "src/assets/images/ReviewCategories/Imag2.png" },
-  { title: "Планшеты", img: "src/assets/images/ReviewCategories/imag3.png" },
-  { title: "Умные часы", img: "src/assets/images/ReviewCategories/Imag4.png" },
-  { title: "Игровые приставки", img: "src/assets/images/ReviewCategories/Imag5.png" },
-  { title: "Наушники", img: "src/assets/images/ReviewCategories/Imag6.png" },
-  { title: "Портативные колон", img: "src/assets/images/ReviewCategories/Imag7.png" },
-  { title: "Аксессуары", img: "src/assets/images/ReviewCategories/Imag8.png" },
-];
+interface Category {
+  id: number;
+  title: string;
+  img: string;
+}
 
 const ReviewCategories: React.FC = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+
+  useEffect(() => {
+    fetch("http://localhost:3000/ReviewCategories")
+      .then((res) => res.json())
+      .then((data: Category[]) => {
+        setCategories(data);
+      })
+  }, []);
+
+
+
   return (
     <section className="best-choice-section">
       <h2 className="title">Категории обзоров</h2>
       <div className="category-grid">
-        {categories.map((item, idx) => (
-          <div className="category-item" key={idx}>
+        {categories.map((item) => (
+          <div className="category-item" key={item.id}>
             <div className="category-card">
               <img src={item.img} alt={item.title} className="category-img" />
             </div>
@@ -27,7 +35,6 @@ const ReviewCategories: React.FC = () => {
         ))}
       </div>
     </section>
-
   );
 };
 
