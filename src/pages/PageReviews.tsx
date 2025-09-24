07fbd17e0dfe2bfe481269206cf4ab2da84e91db
baiel <raimkulovbaiel2@gmail.com>
2025-09-24 22:42:43 +0600

    addedTo-ReviewCategories

--- src/pages/PageReviews.tsx
+++ src/pages/PageReviews.tsx
@@ -1,12 +1,14 @@
 import React from 'react' 
 
 import Search from './PageReviews/Sections/Search/Search'; 
-import ReviewCategories from './PageReviews/Sections/ReviewCategories/ReviewCategories';
+import ReviewCategories from './PageReviews/Sections/ReviewCategories/ReviewCategories'; 
+import ProductGrid from './PageReviews/Sections/ProductGrid/ProductGrid';
  const PageReviews = () => {
   return (
     <div> 
         <Search/> 
-        <ReviewCategories/>
+        <ReviewCategories/> 
+        <ProductGrid/>
     </div>
   )
 }
