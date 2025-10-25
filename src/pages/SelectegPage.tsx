7779b578228789b0dd8769fabc5af103682037f8
baiel <raimkulovbaiel2@gmail.com>
2025-10-25 20:29:19 +0600

    refact-CameraAutonomy

--- src/pages/SelectegPage.tsx
+++ src/pages/SelectegPage.tsx
@@ -2,11 +2,13 @@
 import PostPage from './SelectedPage/Sections/PostPage/PostPage'; 
 import RenderSmartphone from './SelectedPage/Sections/RenderSmartphone/RenderSmartphone'; 
 import Presentation from './SelectedPage/Sections/Presentation/Presentation'; 
-import ProcessorSpec from './SelectedPage/Sections/ProcessorSpec/ProcessorSpec';
+import ProcessorSpec from './SelectedPage/Sections/ProcessorSpec/ProcessorSpec'; 
+import CameraAutonomy from './SelectedPage/Sections/CameraAutonomy/CameraAutonomy';
  const SelectegPage = () => {
   return (
     <><PostPage /><RenderSmartphone /> 
-     <Presentation /><ProcessorSpec />
+     <Presentation /><ProcessorSpec /> 
+     <CameraAutonomy />
      
      </>
   )
