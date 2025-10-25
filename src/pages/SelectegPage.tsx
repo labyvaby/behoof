fe9d900ae2bab4ec660f1d6ef7db499e24935147
baiel <raimkulovbaiel2@gmail.com>
2025-10-25 19:21:59 +0600

    refact-Prezentation

--- src/pages/SelectegPage.tsx
+++ src/pages/SelectegPage.tsx
@@ -1,9 +1,12 @@
 //import Footer from  './SelectedPage/Sections/PostPage/PostPage'; 
 import PostPage from './SelectedPage/Sections/PostPage/PostPage'; 
-import RenderSmartphone from './SelectedPage/Sections/RenderSmartphone/RenderSmartphone';
+import RenderSmartphone from './SelectedPage/Sections/RenderSmartphone/RenderSmartphone'; 
+import Presentation from './SelectedPage/Sections/Presentation/Presentation';
  const SelectegPage = () => {
   return (
-    <><PostPage /><RenderSmartphone /></>
+    <><PostPage /><RenderSmartphone /> 
+     <Presentation />
+     </>
   )
 }
  
