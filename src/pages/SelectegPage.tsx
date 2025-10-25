e8a0a77ebafdc10bf08d7e0b676bcd80ee851ff8
baiel <raimkulovbaiel2@gmail.com>
2025-10-25 22:24:55 +0600

    added-CommentsSection

--- src/pages/SelectegPage.tsx
+++ src/pages/SelectegPage.tsx
@@ -3,12 +3,13 @@ import PostPage from './SelectedPage/Sections/PostPage/PostPage';
 import RenderSmartphone from './SelectedPage/Sections/RenderSmartphone/RenderSmartphone'; 
 import Presentation from './SelectedPage/Sections/Presentation/Presentation'; 
 import ProcessorSpec from './SelectedPage/Sections/ProcessorSpec/ProcessorSpec'; 
-import CameraAutonomy from './SelectedPage/Sections/CameraAutonomy/CameraAutonomy';
+import CameraAutonomy from './SelectedPage/Sections/CameraAutonomy/CameraAutonomy'; 
+import CommentsSection from './SelectedPage/Sections/CommentsSection/CommentsSection';
  const SelectegPage = () => {
   return (
     <><PostPage /><RenderSmartphone /> 
      <Presentation /><ProcessorSpec /> 
-     <CameraAutonomy />
+     <CameraAutonomy /><CommentsSection/>
      
      </>
   )
