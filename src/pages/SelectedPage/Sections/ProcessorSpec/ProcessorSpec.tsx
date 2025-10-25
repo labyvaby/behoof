1352e2ecaaae4173bbfa731778ed6a468a6e23fb
baiel <raimkulovbaiel2@gmail.com>
2025-10-25 19:58:07 +0600

    added-ProcessorSpec

---
+++ src/pages/SelectedPage/Sections/ProcessorSpec/ProcessorSpec.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './style.scss';
+
+interface ProcessorItem {
+  id: number;
+  text: string;
+}
+
+const ProcessorSpec: React.FC = () => {
+  const [specs, setSpecs] = useState<ProcessorItem[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/processor")
+      .then(res => res.json())
+      .then(data => setSpecs(data));
+  }, []);
+
+  return (
+    <div className="processor-spec">
+      <h2 className="spec-title">Процессор</h2>
+
+      <div className="spec-content">
+        {specs.map(item => (
+          <p key={item.id} className="spec-item">
+            {item.text}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProcessorSpec;
--- src/pages/SelectegPage.tsx
+++ src/pages/SelectegPage.tsx
@@ -1,11 +1,13 @@
 //import Footer from  './SelectedPage/Sections/PostPage/PostPage'; 
 import PostPage from './SelectedPage/Sections/PostPage/PostPage'; 
 import RenderSmartphone from './SelectedPage/Sections/RenderSmartphone/RenderSmartphone'; 
-import Presentation from './SelectedPage/Sections/Presentation/Presentation';
+import Presentation from './SelectedPage/Sections/Presentation/Presentation'; 
+import ProcessorSpec from './SelectedPage/Sections/ProcessorSpec/ProcessorSpec';
  const SelectegPage = () => {
   return (
     <><PostPage /><RenderSmartphone /> 
-     <Presentation />
+     <Presentation /><ProcessorSpec />
+     
      </>
   )
 }
